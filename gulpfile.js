ac10bf5ae4ce6eabde588b26ef1615533a9e640a
DovDe <dovdekeyser@yahoo.com>
2017-11-21 13:35:52 -0800

    Add gulp sass and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,14 @@
-node_modules
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+
+// this task takes the app.scss and compiles it to the
+//app/css folder
+gulp.task('sass', function() {
+  return gulp.src('src/scss/app.scss')
+    .pipe(sass({outputStyle: 'expanded'}).on('error',sass.logError))
+    .pipe(gulp.dest('app/css'));
+});
+
+// this task will tell gulp which tasks to run
+gulp.task('default', ['sass']);
