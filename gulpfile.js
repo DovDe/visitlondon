0549630a72fc31e01d9971a136ea03f03817169f
DovDe <dovdekeyser@yahoo.com>
2017-11-23 22:30:14 -0800

    added copy task to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,9 @@ var autoprefixer       = require('gulp-autoprefixer');
 var SOURCEPATHS = {
   //the astrisc means that any file with the extention after it will be checked
 // this is defining the sass source
-  sassSource:'src/scss/*.scss'
+  sassSource:'src/scss/*.scss',
+  // here I am adding all the html files
+  htmlSource: 'src/*.html'
 }
 
 // variable app folder
@@ -27,8 +29,7 @@ gulp.task('sass', function() {
 // adding autoprefixer this will automatically add
 // browser prefixes to make the app supproted across
 // all browsers
-    .pipe(autoprefixer());
-
+    .pipe(autoprefixer())
   //  this pipe is for the sass it defines how we are gong to
   //the sass the outputStyle is currently on expanded but for
   // development I will use compressed which will minify the css
@@ -38,6 +39,17 @@ gulp.task('sass', function() {
 //the scss and prfixes are added
     .pipe(gulp.dest(APPPATH.css));
 });
+
+
+
+gulp.task('copy', function(){
+  gulp.src(SOURCEPATHS.htmlSource)
+    .pipe(gulp.dest(APPPATH.root));
+});
+
+
+
+
 
 // this is called serve because we are using this with browserSync
 // to create a server for us.
@@ -53,11 +65,13 @@ gulp.task('serve', ['sass'], function(){
 });
 
 //  Watch method
-gulp.task('watch', ['serve', 'sass'], function(){
+gulp.task('watch', ['serve', 'sass', 'copy'], function(){
 // the first brakets in this method defines what to listen to
 // the second brakets in the method defines what to run once the
 // where changes
   gulp.watch([SOURCEPATHS.sassSource],['sass']);
+//
+  gulp.watch([SOURCEPATHS.htmlSource],['copy']);
 });
 // this task will tell gulp which tasks to run
 gulp.task('default', ['watch']);
